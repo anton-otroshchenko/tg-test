dfdc33ec6e5dddf54c5b47771af9779d766b455f
anton-otroshchenko <anton3447@gmail.com>
2024-05-30 17:20:23 +0300

    test

--- src/slices/user/actions.ts
+++ src/slices/user/actions.ts
@@ -11,7 +11,7 @@ const getUser = createAsyncThunk(`${sliceName}/get-user`, async () => {
         console.error(e);
         initDataRaw = window.Telegram?.WebApp.initData;
     }
-    const response = await fetch('https://dcc3-93-170-66-83.ngrok-free.app/api/user', {
+    const response = await fetch('https://ac99-93-170-66-83.ngrok-free.app/api/user', {
         headers: {
             "Content-Type": "text/plain",
             Authorization: `tma ${initDataRaw}`,
